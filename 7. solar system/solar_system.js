78ca9b33209aa7e41005c6fc2ace6418b028d3b1
egall <a01745776@itesm.mx>
2021-04-22 18:08:42 -0500

    Comments

--- 7. solar system/solar_system.js
+++ 7. solar system/solar_system.js
@@ -1,128 +1,185 @@
-class Planet{
-  constructor(radius, mapUrl, orbitRadius){
+class Planet {
+  /**
+   *
+   * @param {int} radius
+   * @param {string} mapUrl
+   * @param {int} orbitRadius
+   */
+  constructor(radius, mapUrl, orbitRadius) {
     this.radius = radius;
     this.parentGroup = GENERAL_GROUP;
     this.material = this.createMaterial(mapUrl);
-    this.rotationGroup = new THREE.Object3D();
-    this.moonsGroup = new THREE.Object3D();
+    this.rotationGroup = new THREE.Object3D(); // Grupo para manejar la traslacion
+    this.moonsGroup = new THREE.Object3D(); // Grupo para rotacion y lunas
     this.orbitRadius = orbitRadius;
-    this.position = this.calculatePosition();
-    this.moonsArray = [];
+    this.position = this.calculatePosition(); // Posicion calculada con respecto a su orbita
+    this.moonsArray = []; // Arreglo para guardar las lunas del planeta y animarlas
     this.drawOrbit();
     this.draw();
   }
 
-  draw(){
-    this.geometry = new THREE.SphereGeometry( this.radius, 32, 32 );
+  draw() {
+    this.geometry = new THREE.SphereGeometry(this.radius, 32, 32);
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.rotationGroup.updateMatrixWorld();
-    this.moonsGroup.position.set(this.position.x, this.position.y, this.position.z);
-    this.moonsGroup.add(this.mesh)    
+    this.moonsGroup.position.set(
+      this.position.x,
+      this.position.y,
+      this.position.z
+    );
+    this.moonsGroup.add(this.mesh);
     this.rotationGroup.add(this.moonsGroup);
     this.parentGroup.add(this.rotationGroup);
   }
 
-  drawOrbit(){
-    this.orbit = new THREE.RingGeometry( this.orbitRadius - 1, this.orbitRadius + 1, 45 );
-    this.orbit.rotateX(Math.PI/2);
-    let orbitMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } )
-    let orbitMesh = new THREE.Mesh( this.orbit, orbitMaterial );
-    GENERAL_GROUP.add(orbitMesh)
+  drawOrbit() {
+    this.orbit = new THREE.RingGeometry(
+      this.orbitRadius - 1,
+      this.orbitRadius + 1,
+      45
+    ); // Orbita en forma de anillo
+    this.orbit.rotateX(Math.PI / 2); // Rotacion a 90° para quedar en el plano XZ
+    let orbitMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide,
+    });
+    let orbitMesh = new THREE.Mesh(this.orbit, orbitMaterial);
+    GENERAL_GROUP.add(orbitMesh);
   }
 
-  createMoons(moons, moonRadius){
-    let moonGeometry = new THREE.SphereGeometry( moonRadius, 32, 32 ),
-    moonMaterial = this.createMaterial("../images/solar_system/moon/moonbump2k.jpg"),
-    moonPositionOptions = [{x:8,y:5,z:8},{x:-8,y:-4,z:-8},{x:8,y:3,z:-8},{x:-8,y:1,z:8},{x:9,y:-5,z:-6}],
-    posFactor = this.radius/10;
+  createMoons(moons, moonRadius) {
+    let moonGeometry = new THREE.SphereGeometry(moonRadius, 32, 32),
+      moonMaterial = this.createMaterial(
+        "../images/solar_system/moon/moonbump2k.jpg"
+      ),
+      moonPositionOptions = [
+        { x: 8, y: 5, z: 8 },
+        { x: -8, y: -4, z: -8 },
+        { x: 8, y: 3, z: -8 },
+        { x: -8, y: 1, z: 8 },
+        { x: 9, y: -5, z: -6 },
+      ], // Posiciones arbitrarias para las lunas
+      posFactor = this.radius / 10; // Operacion para evitar que las lunas choquen con sus planetas
 
-    for (let index = 0; index < moons; index++){
-      let moonMesh = new THREE.Mesh(moonGeometry, moonMaterial)
-      moonMesh.position.set(moonPositionOptions[index].x * posFactor,moonPositionOptions[index].y* posFactor,moonPositionOptions[index].z* posFactor)
-      this.moonsGroup.add(moonMesh)
-      this.moonsArray.push(moonMesh)
+    for (let index = 0; index < moons; index++) {
+      let moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+      moonMesh.position.set(
+        moonPositionOptions[index].x * posFactor,
+        moonPositionOptions[index].y * posFactor,
+        moonPositionOptions[index].z * posFactor
+      );
+      this.moonsGroup.add(moonMesh);
+      this.moonsArray.push(moonMesh);
     }
-  }  
+  }
 
-  createMaterial = (url) => new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(url) });
+  createMaterial = (url) =>
+    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(url) });
 
-  getRandomCoords = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  getRandomCoords = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
 
   calculatePosition = () => {
+    /*
+     *  La posicion inicial del planeta se calcula de forma aleatoria,
+     *  dentro de la trayectoria de su orbita respecto al sol
+     */
     let x = this.getRandomCoords(-this.orbitRadius, this.orbitRadius),
-    y = this.getRandomCoords(-2,2),
-    pos = Math.floor(Math.random()*2),
-    coeficiente = Math.pow(this.orbitRadius,2) - Math.pow(x,2),
-    z = undefined;
-    if(pos === 1){
-      z = Math.sqrt(coeficiente)
-    }else{
-      z = -Math.sqrt(coeficiente)
+      y = this.getRandomCoords(-2, 2),
+      pos = Math.floor(Math.random() * 2),
+      coeficiente = Math.pow(this.orbitRadius, 2) - Math.pow(x, 2),
+      z = undefined;
+    if (pos === 1) {
+      z = Math.sqrt(coeficiente);
+    } else {
+      z = -Math.sqrt(coeficiente);
     }
-    return {x:x,y:y,z:z};
+    return { x: x, y: y, z: z };
+  };
+
+  addRing() {
+    /**
+     * Metodo para agregar anillos a los planetas (Saturno)
+     */
+    let ringGeometry = new THREE.TorusGeometry(this.radius + 5, 3, 16, 100);
+    ringGeometry.rotateX(Math.PI / 2);
+    let ringMaterial = new THREE.MeshBasicMaterial({ color: 0x420420 });
+    let ring = new THREE.Mesh(ringGeometry, ringMaterial);
+    this.moonsGroup.add(ring);
   }
 
-  addRing(){
-    let ringGeometry = new THREE.TorusGeometry( this.radius + 5, 3, 16, 100 );
-    ringGeometry.rotateX(Math.PI/2)
-    let ringMaterial = new THREE.MeshBasicMaterial( { color: 0x420420 } );
-    let ring = new THREE.Mesh( ringGeometry, ringMaterial );
-    this.moonsGroup.add(ring)
-  }
-
-  animate(angle){
+  animate(angle) {
+    /**
+     * Metodo para animar al plantea (rotacion) y sus lunas (rotacion y traslacion)
+     */
     this.mesh.rotation.y += angle;
-    this.moonsArray.forEach(moonMesh => {
+    this.moonsArray.forEach((moonMesh) => {
       moonMesh.rotation.y += angle;
     });
-    this.moonsGroup.rotation.y -= angle/3;
+    this.moonsGroup.rotation.y -= angle / 3;
   }
 }
 
-class Asteroid{
-  constructor(radius, orbitRadius){
+class Asteroid {
+  /**
+   *
+   * @param {int} radius
+   * @param {int} orbitRadius
+   */
+  constructor(radius, orbitRadius) {
     this.radius = radius;
     this.parentGroup = ASTEROID_GROUP;
-    this.material = this.createMaterial("../images/solar_system/asteroid/asteroid.jpg");
+    this.material = this.createMaterial(
+      "../images/solar_system/asteroid/asteroid.jpg"
+    );
     this.rotationGroup = new THREE.Object3D();
     this.orbitRadius = orbitRadius;
     this.position = this.calculatePosition();
     this.draw();
   }
 
-  draw(){
-    this.geometry = new THREE.SphereGeometry( this.radius, 32, 32 );
+  draw() {
+    this.geometry = new THREE.SphereGeometry(this.radius, 32, 32);
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.rotationGroup.updateMatrixWorld();
-    this.rotationGroup.position.set(this.position.x, this.position.y, this.position.z);
-    this.rotationGroup.add(this.mesh)
+    this.rotationGroup.position.set(
+      this.position.x,
+      this.position.y,
+      this.position.z
+    );
+    this.rotationGroup.add(this.mesh);
     this.parentGroup.add(this.rotationGroup);
   }
 
-  createMaterial = (url) => new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(url) });
+  createMaterial = (url) =>
+    new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load(url) });
 
-  getRandomCoords = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  getRandomCoords = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
 
   calculatePosition = () => {
+    /*
+     *  La posicion inicial del asteroide se calcula de forma aleatoria,
+     *  dentro de la trayectoria de su orbita respecto al sol
+     */
     let x = this.getRandomCoords(-this.orbitRadius, this.orbitRadius),
-    y = this.getRandomCoords(-2,2),
-    pos = Math.floor(Math.random()*2),
-    coeficiente = Math.pow(this.orbitRadius,2) - Math.pow(x,2),
-    z = undefined;
-    if(pos === 1){
-      z = Math.sqrt(coeficiente)
-    }else{
-      z = -Math.sqrt(coeficiente)
+      y = this.getRandomCoords(-2, 2),
+      pos = Math.floor(Math.random() * 2),
+      coeficiente = Math.pow(this.orbitRadius, 2) - Math.pow(x, 2),
+      z = undefined;
+    if (pos === 1) {
+      z = Math.sqrt(coeficiente);
+    } else {
+      z = -Math.sqrt(coeficiente);
     }
-    return {x:x,y:y,z:z};
-  }
+    return { x: x, y: y, z: z };
+  };
 }
 
 // Global consts
 const DURATION = 5000, // ms
   GENERAL_GROUP = new THREE.Object3D(),
-  ASTEROID_GROUP = new THREE.Object3D()
-  
+  ASTEROID_GROUP = new THREE.Object3D();
 
 // Global vars
 let renderer = null,
@@ -152,17 +209,17 @@ function animate() {
   planets.forEach((planet) => {
     planet.animate(angle);
   });
-  let rotationGroups = GENERAL_GROUP.children,
-  slowing_rate = 1,
-  rotationGroups2 = rotationGroups.filter(checkObjects);
-  rotationGroups2.forEach(group => {
-    group.rotation.y += angle / slowing_rate
-    slowing_rate += 2
+  let rotationGroups = GENERAL_GROUP.children, // Grupos a trasladar
+    slowing_rate = 1, // Variable para variar la velocidad de rotacion entre planetas
+    rotationGroups2 = rotationGroups.filter(checkObjects); // filtrar entre elementos a trasladar
+  rotationGroups2.forEach((group) => {
+    group.rotation.y += angle / slowing_rate;
+    slowing_rate += 2;
   });
 }
 
-function checkObjects(obj){
-  return obj.type === "Object3D"
+function checkObjects(obj) {
+  return obj.type === "Object3D";
 }
 
 function run() {
@@ -200,16 +257,15 @@ function createScene(canvas) {
   );
   camera.position.z = 40;
   scene.add(camera);
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+
+  // Herramienta para controlar la escena con ayuda del mouse
+  controls = new THREE.OrbitControls(camera, renderer.domElement); 
+  controls.enableDamping = true;
   controls.dampingFactor = 0.05;
-
   controls.screenSpacePanning = false;
-
-  controls.minDistance =200;
+  controls.minDistance = 150;
   controls.maxDistance = 700;
-
-  controls.maxPolarAngle = Math.PI ;
+  controls.maxPolarAngle = Math.PI; // 180° como rotacion maxima
 
   // Add a directional light to show off the objects
   let light = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -227,53 +283,57 @@ function createScene(canvas) {
   // Now add the group to our scene
   scene.add(GENERAL_GROUP);
 
-  // Crear Sol
+  // Ruta a las texturas de los planteas
   let sunMapUrl = "../images/solar_system/sun/sun.jpg",
-  mercuryMapUrl = "../images/solar_system/mercury/mercurymap.jpg",
-  venusMapUrl = "../images/solar_system/venus/venusmap.jpg",
-  earthMapUrl = "../images/solar_system/earth/earthmap1k.jpg",
-  marsMapUrl = "../images/solar_system/mars/mars_1k_color.jpg",
-  jupiterMapUrl = "../images/solar_system/jupiter/jupitermap.jpg",
-  saturnMapUrl = "../images/solar_system/saturn/saturnmap.jpg",
-  uranusMapUrl = "../images/solar_system/uranus/uranusmap.jpg",
-  neptuneMapUrl = "../images/solar_system/neptune/neptunemap.jpg",
-  plutoMapUrl = "../images/solar_system/pluto/plutomap2k.jpg";
+    mercuryMapUrl = "../images/solar_system/mercury/mercurymap.jpg",
+    venusMapUrl = "../images/solar_system/venus/venusmap.jpg",
+    earthMapUrl = "../images/solar_system/earth/earthmap1k.jpg",
+    marsMapUrl = "../images/solar_system/mars/mars_1k_color.jpg",
+    jupiterMapUrl = "../images/solar_system/jupiter/jupitermap.jpg",
+    saturnMapUrl = "../images/solar_system/saturn/saturnmap.jpg",
+    uranusMapUrl = "../images/solar_system/uranus/uranusmap.jpg",
+    neptuneMapUrl = "../images/solar_system/neptune/neptunemap.jpg",
+    plutoMapUrl = "../images/solar_system/pluto/plutomap2k.jpg";
 
-  const sun = new Planet(30, sunMapUrl, 0)
-  
-  const mercury = new Planet(5, mercuryMapUrl, 40)
-  planets.push(mercury)
-  const venus = new Planet(6, venusMapUrl, 55)
-  planets.push(venus)
-  const earth = new Planet(10, earthMapUrl, 78) // 1
-  earth.createMoons(1, 3);
-  planets.push(earth)
-  const mars = new Planet(8, marsMapUrl, 105)
-  planets.push(mars)
-  createAsteroids();
-  const jupiter = new Planet(17, jupiterMapUrl, 150) // 5
-  jupiter.createMoons(5, 2)
-  planets.push(jupiter)
-  const saturn = new Planet(12, saturnMapUrl, 190)
-  saturn.addRing();
-  planets.push(saturn)
-  const uranus = new Planet(10, uranusMapUrl, 218)
-  planets.push(uranus)
-  const neptune = new Planet(11, neptuneMapUrl, 245)
-  planets.push(neptune)
-  const pluto = new Planet(5, plutoMapUrl, 270)
-  planets.push(pluto)
+  // Construccion de los planetas
+  const sun = new Planet(30, sunMapUrl, 0);
+  const mercury = new Planet(5, mercuryMapUrl, 40);
+  planets.push(mercury);
+  const venus = new Planet(6, venusMapUrl, 55);
+  planets.push(venus);
+  const earth = new Planet(10, earthMapUrl, 78);
+  earth.createMoons(1, 3); // Creacion de 1 luna para la tierra
+  planets.push(earth);
+  const mars = new Planet(8, marsMapUrl, 105);
+  planets.push(mars);
+  createAsteroids(); // Creacion del cinturon de asteroides
+  const jupiter = new Planet(17, jupiterMapUrl, 150);
+  jupiter.createMoons(5, 2); // Creacion de 5 lunas para Jupiter
+  planets.push(jupiter);
+  const saturn = new Planet(12, saturnMapUrl, 190);
+  saturn.addRing(); // Creacion del anillo de Saturno
+  planets.push(saturn);
+  const uranus = new Planet(10, uranusMapUrl, 218);
+  planets.push(uranus);
+  const neptune = new Planet(11, neptuneMapUrl, 245);
+  planets.push(neptune);
+  const pluto = new Planet(5, plutoMapUrl, 270);
+  planets.push(pluto);
 }
 
-function createAsteroids(){
+function createAsteroids() {
   for (let j = 0; j < 50; j++) {
-    new Asteroid(2, 115);
+    // Se manejan dos distancias entre los asteoides y el sol
+    new Asteroid(2, 115); 
     new Asteroid(2, 130);
-    let orbit = new THREE.RingGeometry( 119, 121, 45 );
-    orbit.rotateX(Math.PI/2);
-    let orbitMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } )
-    let orbitMesh = new THREE.Mesh( orbit, orbitMaterial );
+    let orbit = new THREE.RingGeometry(119, 121, 45); // Orbita del cinturon de asteroides
+    orbit.rotateX(Math.PI / 2);
+    let orbitMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide,
+    });
+    let orbitMesh = new THREE.Mesh(orbit, orbitMaterial);
     GENERAL_GROUP.add(ASTEROID_GROUP);
-    GENERAL_GROUP.add(orbitMesh)
+    GENERAL_GROUP.add(orbitMesh);
   }
 }
